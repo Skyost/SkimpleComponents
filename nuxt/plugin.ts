21b98c7532a1f766ae4a661a12676b27674b7bbe
Hugo Delaunay <Skyost@users.noreply.github.com>
2022-07-29 01:07:40 +0200

    Fixed a bug with the Nuxt plugin.

--- nuxt/plugin.ts
+++ nuxt/plugin.ts
@@ -1,9 +1,11 @@
+// Warnings are ignored here because this file is meant to be imported by Nuxt.
 import * as bootstrap from 'bootstrap'
-
-// Those ignores allow use to not include Nuxt.js dependency.
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-// eslint-disable-next-line no-undef
+import { defineNuxtPlugin } from '#app'
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.provide('bootstrap', bootstrap)
 })
